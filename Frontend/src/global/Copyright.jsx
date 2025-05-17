1086487264800274aa0710f835051da1385de41e
Pranay Fadtare <114424610+PranayFadtare@users.noreply.github.com>
2025-05-17 18:01:17 +0530

    Update Copyright.jsx

--- Frontend/src/global/Copyright.jsx
+++ Frontend/src/global/Copyright.jsx
@@ -9,7 +9,7 @@ export default function Copyright(props) {
     return (
       <Typography variant="body2" color="text.primary" align="center" {...props}>
         <Link color="inherit" sx={{textDecoration:"none"}} href="https://github.com/neu-mis-info6150-fall-2022/final-project-jams/tree/main">
-          Made with Love by Jay, Anay, Moinuddin & Swapnil
+          Made by Darshan Bardeo, Atharva Bambere and Arjavi Kakulate
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
